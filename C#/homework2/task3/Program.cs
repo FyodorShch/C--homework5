eb89102560acc7374605c36ae99d003228140dce
Fyodor <feodorinin@gmail.com>
2023-01-08 13:31:09 +0300

    add solutions for 3 tasks

---
+++ C#/homework2/task3/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Clear();
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if(Math.Abs(a)<6)
+{
+    Console.WriteLine("Будни :(");
+}
+else
+{
+Console.WriteLine("Выходной :D");
+}
